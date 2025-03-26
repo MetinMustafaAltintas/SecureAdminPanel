c5d7e0224a3db36fc9cf6e7235b1721ba8634985
Metin Mustafa Altıntaş <metinmustafaaltintas@gmail.com>
2025-03-26 15:17:37 +0300

    Added user registration page with form validation, error handling, and Google/Facebook signup options. Updated middleware for form validation and created new EJS templates for registration and layout.
    Bu commit, kullanıcı kayıt sayfasını tanıtmaktadır. Kayıt formu için ön yüz doğrulaması, doğrulama hataları için hata işleme ve Google/Facebook ile kayıt seçeneklerinin entegrasyonu dahil edilmiştir. Form doğrulama için middleware, kullanıcı girişi doğrulamasını daha verimli bir şekilde işlemek için güncellenmiştir. Ayrıca, kayıt sayfası ve layout yapısı için tutarlı bir tasarım sağlamak amacıyla yeni EJS şablonları oluşturulmuştur.

--- app.js
+++ app.js
@@ -1,20 +1,58 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const app = express();
+const session = require('express-session');
+const flash = require('connect-flash');
+
+// routerler include edilir
+const authRouter = require('./src/routers/auth_router');
 
 // DB bağlantısı
 require('./src/config/database');
 
+// session ve flash message
+app.use(session(
+    {
+        secret: process.env.SESSION_SECRET,
+        resave : false,
+        saveUninitialized: true,
+        cookie: {
+            maxAge:1000
+        }
+    }
+));
+
+app.use(flash());
+app.use((req,res,next) => {
+    res.locals.validation_error = req.flash('validation_error');
+    res.locals.email = req.flash('email');
+    res.locals.ad = req.flash('ad');
+    res.locals.soyad = req.flash('soyad');
+    res.locals.sifre = req.flash('sifre');
+    res.locals.resifre = req.flash('resifre');
+
+
+
+    next();
+});
+
+// formdan gelen değerlerin okuna bilmesi için 
+app.use(express.urlencoded({extended: true}));
+
+//template engine ayarları
 const ejs = require('ejs');
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
 app.use(expressLayouts);
+app.use(express.static('public'));
 app.set('view engine' , 'ejs');
 app.set('views', path.resolve(__dirname, './src/views'));
 
 app.get('/' , (req, res) => {
     res.json({ mesaj: 'merhaba'});
 })
+
+app.use('/', authRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`Server ${process.env.PORT} portundan ayaklandı`)
---
+++ src/controllers/auth_controller.js
@@ -0,0 +1,48 @@
+const {validationResult } = require('express-validator');
+
+
+const loginFormunuGoster = (req , res , next) => {
+    res.render('login' , { layout: './layout/auth_layout.ejs'});
+}
+const login = (req , res , next) => {
+    console.log(req.body);
+    res.render('login' , { layout: './layout/auth_layout.ejs'});
+}
+
+const registerFormunuGoster = (req , res , next) => {
+    res.render('register' , { layout: './layout/auth_layout.ejs'});
+}
+const register = (req , res , next) => {
+
+    const hatalar = validationResult(req);
+    if(!hatalar.isEmpty()){
+
+        req.flash('validation_error', hatalar.array());
+        req.flash('email' , req.body.email);
+        req.flash('ad' , req.body.ad);
+        req.flash('soyad' , req.body.soyad);
+        req.flash('sifre' , req.body.sifre);
+        req.flash('resifre' , req.body.resifre);
+        res.redirect('/register');
+
+    }
+}
+
+
+const forgetPasswordFormunuGoster = (req , res , next) => {
+    res.render('forget_password' , { layout: './layout/auth_layout.ejs'});
+}
+
+const forgetPassword = (req , res , next) => {
+    console.log(req.body);
+    res.render('forget_password' , { layout: './layout/auth_layout.ejs'});
+}
+
+module.exports = {
+    loginFormunuGoster,
+    registerFormunuGoster,
+    forgetPasswordFormunuGoster,
+    register,
+    login,
+    forgetPassword
+}
